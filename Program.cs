8665a0bcc2ed5908a12005f45a31e29de72fd34e
CHOI SUJIN <amor77777777778@gmail.com>
2018-08-16 22:08:40 +0900

    Earth is mine

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace HelloWorld
         {
             Console.WriteLine("Hello World");
             Console.WriteLine("World is mine");
+            Console.WriteLine("Earth is mine");
         }
     }
 }
